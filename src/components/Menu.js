a9d57cd92f12c8f843594d0dc80db68fd30d1c03
mahaliroblesarbieto <mahaliroblesarbieto@gmail.com>
2019-04-05 17:41:05 -0500

    modificando developer

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,65 +1,43 @@
 import React from 'react';
-import menu from '../data/menu'
 
 class Menu extends React.Component {
 
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      menu: menu
-    }
+      items: [],
+      isLoaded: false,
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://raw.githubusercontent.com/mahaliroblesarbieto/LIM008-fe-burger-queen/prototype/src/data/menu.json')
+      .then(res => res.json())
+      .then((json) => {
+        this.setState({
+          isLoaded: true,
+          items: json,
+        });
+      });
   }
 
   render() {
-    const menu = this.state.menu.menu.map((item, i) => {
-      return (
-        <div>
-          <button>{item.title} - {item.value}</button>
-        </div>
-      )
-    })
+    const { isLoaded, items } = this.state;
+    if (!isLoaded) {
+      return <div>Loading...</div>;
+    }
 
     return (
       <div>
-        {menu}
-      </div>  
-      )
+        {items.map(item => (
+          <div>
+            {item.name}
+            {item.value}
+            <button type="button">+</button>
+          </div>
+        ))}
+      </div>
+    );
   }
-    // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     items: [],
-  //     isLoaded: false,
-  //   }
-  // }
-
-  // componentDidMount() {
-  //   fetch('https://raw.githubusercontent.com/mahaliroblesarbieto/LIM008-fe-burger-queen/developer/src/data/menu.json')
-  //   .then(res => res.json())
-  //   .then(json => {
-  //     this.setState({
-  //       isLoaded: true,
-  //       items:json,
-  //     })
-  //   })
-  // }
-  // render() {
-  //   let{isLoaded, items} = this.state;
-  //   if(!isLoaded) {
-  //     return <div>Loading...</div>
-  //   }
-  //   else {
-  //     return(
-  //       <div>
-  //         <ul>
-  //           {items.map(item => (
-  //           <li key={item.title}>
-  //           {item.value}</li>))}
-  //         </ul>
-  //       </div>
-  //     )
-  //   }
-   
-  // }
 }
 export default Menu;

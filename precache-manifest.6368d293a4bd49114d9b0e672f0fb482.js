764b3b76bb72f5ae076b3d71022b4e81acce45f1
mahaliroblesarbieto <mahaliroblesarbieto@gmail.com>
2019-04-19 23:43:39 -0500

    Updates

--- precache-manifest.6368d293a4bd49114d9b0e672f0fb482.js
+++ precache-manifest.6368d293a4bd49114d9b0e672f0fb482.js
@@ -4,23 +4,23 @@ self.__precacheManifest = [
     "url": "/LIM008-fe-burger-queen/static/js/runtime~main.10776b7b.js"
   },
   {
-    "revision": "cd61f7c7ae3106cb28b1",
-    "url": "/LIM008-fe-burger-queen/static/js/main.f867ebe4.chunk.js"
+    "revision": "51ae2333a4462b94510f",
+    "url": "/LIM008-fe-burger-queen/static/js/main.b8126b94.chunk.js"
   },
   {
     "revision": "2b0b6dcf87fc51d0c8bb",
     "url": "/LIM008-fe-burger-queen/static/js/2.e467a123.chunk.js"
   },
   {
-    "revision": "cd61f7c7ae3106cb28b1",
-    "url": "/LIM008-fe-burger-queen/static/css/main.7931f78b.chunk.css"
+    "revision": "51ae2333a4462b94510f",
+    "url": "/LIM008-fe-burger-queen/static/css/main.4354f581.chunk.css"
   },
   {
     "revision": "2b0b6dcf87fc51d0c8bb",
     "url": "/LIM008-fe-burger-queen/static/css/2.16434e7e.chunk.css"
   },
   {
-    "revision": "08d70ff7188675a5a1ef6dc4a27c9bd5",
+    "revision": "80ed43e39ba6789e8f6b4040eff7f92a",
     "url": "/LIM008-fe-burger-queen/index.html"
   }
 ];

89b6d2aa862b2d124b2f8e5f99d87efb9d3af0ee
mahaliroblesarbieto <mahaliroblesarbieto@gmail.com>
2019-05-30 00:28:49 -0500

    proyecto final

---
+++ build/precache-manifest.c035f13988bb0707b3b692effb4b7ad5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a4f816d571dde5ac940",
+    "url": "/LIM008-fe-burger-queen/static/js/runtime~main.10776b7b.js"
+  },
+  {
+    "revision": "c1436ac7bf3040496bb8",
+    "url": "/LIM008-fe-burger-queen/static/js/main.a977e2e3.chunk.js"
+  },
+  {
+    "revision": "8b2076b1787d6a1acb39",
+    "url": "/LIM008-fe-burger-queen/static/js/2.e560822f.chunk.js"
+  },
+  {
+    "revision": "c1436ac7bf3040496bb8",
+    "url": "/LIM008-fe-burger-queen/static/css/main.d9ab8bdc.chunk.css"
+  },
+  {
+    "revision": "8b2076b1787d6a1acb39",
+    "url": "/LIM008-fe-burger-queen/static/css/2.16434e7e.chunk.css"
+  },
+  {
+    "revision": "44c49e022e233476f98e334ff49d96c3",
+    "url": "/LIM008-fe-burger-queen/index.html"
+  }
+];
--- src/index.js
+++ src/index.js
@@ -1,18 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
 import * as serviceWorker from './serviceWorker';
-import BadgeNew from './pages/BadgeNew';
+import App from './components/App';
 import 'bootstrap/dist/css/bootstrap.css';
 
 const container = document.getElementById('root');
-ReactDOM.render(<BadgeNew />, container);
+ReactDOM.render(<App className="width-total" />, container);
 
-
-// ReactDOM.render(<App firstOption = "Ingresar nombre del cliente:" secondOption = "Ingresar número de mesa"/>, container);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
